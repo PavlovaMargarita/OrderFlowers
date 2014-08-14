6ba10606afb18d216b751cf919508858fe3e36fb
Sasha_Morozov <sasha25121993@mail.ru>
2014-08-14 22:51:44 +0300

    OrderDAO

--- Server/src/main/java/bl/dao/order/OrderDAO.java
+++ Server/src/main/java/bl/dao/order/OrderDAO.java
@@ -1,7 +1,13 @@
 package bl.dao.order;
 
+import entity.Order;
+
 /**
  * Created by Margarita on 14.08.2014.
  */
 public interface OrderDAO {
+    public Integer createOrder(Order order);  //return id(Integer) new order
+    public boolean deleteOrder(int id); //if the order is deleted, the method returns true, otherwise false
+    public Order readOrder(int id); //if the order is not found, returns null
+    public void updateOrder(Order order);
 }
--- Server/src/main/java/bl/dao/order/OrderDAOImpl.java
+++ Server/src/main/java/bl/dao/order/OrderDAOImpl.java
@@ -1,15 +1,97 @@
 package bl.dao.order;
 
+import entity.Contact;
+import entity.Order;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Created by Margarita on 14.08.2014.
  */
 public class OrderDAOImpl implements OrderDAO {
     private static OrderDAOImpl ourInstance = new OrderDAOImpl();
+    private SessionFactory factory;
 
     public static OrderDAOImpl getInstance() {
         return ourInstance;
     }
 
     private OrderDAOImpl() {
+        factory = new Configuration().configure().buildSessionFactory();
+    }
+
+    public Integer createOrder(Order order) {
+        if (order == null){
+            throw new NullPointerException("order is null");
+        }
+
+        Integer id = null;
+        Session session = null;
+
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            id = (Integer) session.save(order);
+            session.getTransaction().commit();
+        }
+        finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return id;
+    }
+    public boolean deleteOrder(int id) {
+        boolean result = false;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            Order order = (Order) session.get(Order.class, id);
+            if (order != null){
+                session.delete(order);
+                result = true;
+            }
+            session.getTransaction().commit();
+        }
+        finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return result;
+    }
+    public Order readOrder(int id) {
+        Order order = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            order = (Order) session.get(Order.class, id);
+        }
+        finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return order;
+    }
+    public void updateOrder(Order order) {
+        if (order == null){
+            throw new NullPointerException("contact is null");
+        }
+
+        Session session = null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            session.update(order);
+            session.getTransaction().commit();
+        }
+        finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 }
