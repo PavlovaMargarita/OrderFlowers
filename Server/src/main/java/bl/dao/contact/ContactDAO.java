961ba3dcfb17fdbee4562075eb129c9364812bbb
User <harlex23@gmail.com>
2014-08-14 20:56:33 +0300

    contact<--->database

--- Server/src/main/java/bl/dao/contact/ContactDAO.java
+++ Server/src/main/java/bl/dao/contact/ContactDAO.java
@@ -4,8 +4,8 @@ import entity.Contact;
 
 
 public interface ContactDAO {
-    public Integer createContact(Contact contact);
-    public boolean deleteContact(int id);
-    public Contact readContact(int id);
+    public Integer createContact(Contact contact);  //return id(Integer) new contact
+    public boolean deleteContact(int id); //if the contact is deleted, the method returns true, otherwise false
+    public Contact readContact(int id); //if the contact is not found, returns null
     public void updateContact(Contact contact);
 }
