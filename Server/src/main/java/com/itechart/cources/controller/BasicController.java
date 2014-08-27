b1da2da288713803a8eb25b119d818dc97ef5650
Rita <xomrita@gmail.com>
2014-08-27 12:35:15 +0300

    read login and password

--- Server/src/main/java/com/itechart/cources/controller/BasicController.java
+++ Server/src/main/java/com/itechart/cources/controller/BasicController.java
@@ -1,10 +1,18 @@
 package com.itechart.cources.controller;
 
+import com.itechart.cources.entity.User;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  * Created by Александр on 21.08.2014.
@@ -24,10 +32,32 @@ public class BasicController {
         return "index";
     }
 
-    /*@RequestMapping(method = RequestMethod.POST, value = "/autorize")
-    public @ResponseBody String autorization(HttpServletRequest request) {
-        String login = (String)request.getAttribute("login");
-        String password = request.getParameter("password");
-        return login + password;
-    }*/
+    @RequestMapping(method = RequestMethod.POST, value = "/authorize")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public @ResponseBody String authorization(HttpServletRequest request) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = request.getReader();
+            String line = null;
+            while ((line = reader.readLine()) != null){
+                builder.append(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject jsonObject = null;
+        String login = null;
+        String password = null;
+        try {
+            jsonObject = new JSONObject(builder.toString());
+            login = jsonObject.getString("login");
+            password = jsonObject.getString("password");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return login + " " + password;
+
+    }
 }
