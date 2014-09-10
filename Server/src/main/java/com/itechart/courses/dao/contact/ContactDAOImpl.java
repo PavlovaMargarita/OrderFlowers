0945a72b71483743237173e18f09572785d3a19d
alex-harmaza <harlex23@gmail.com>
2014-09-10 13:03:45 +0300

    Update ContactDAOImpl.java

--- Server/src/main/java/com/itechart/courses/dao/contact/ContactDAOImpl.java
+++ Server/src/main/java/com/itechart/courses/dao/contact/ContactDAOImpl.java
@@ -121,4 +121,96 @@ public class ContactDAOImpl implements ContactDAO {
             }
         }
     }
+    
+    public List<Contact> searchContact(ContactSearchDTO parameters){
+    if (parameters == null){
+        throw new NullPointerException("parameters is null");
+    }
+    Session session = null;
+    List<Contact> contacts = null;
+    try
+    {
+        session = sessionFactory.openSession();
+        StringBuilder builder = new StringBuilder();
+        String surname = parameters.getSurname();
+        String name = parameters.getName();
+        String patronymic = parameters.getPatronymic();
+        Date lowerDateOfBirth = parameters.getLowerDateOfBirth();
+        Date upperDateOfBirth = parameters.getUpperDateOfBirth();
+        String city = parameters.getCity();
+        String street = parameters.getStreet();
+        Integer home = parameters.getHome();
+        Integer flat = parameters.getFlat();
+
+        if (surname != null){
+            builder.append("`surname` = '").append(surname).append("'");
+        }
+        if (name != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`name` = '").append(name).append("'");
+        }
+        if (patronymic != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`patronymic` = '").append(patronymic).append("'");
+        }
+
+        if (lowerDateOfBirth != null && upperDateOfBirth != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`dateOfBirth` BETWEEN '").append(lowerDateOfBirth).append("` AND `").append(upperDateOfBirth).append("'");
+        }
+        else if (lowerDateOfBirth != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`date_of_birth` >= '").append(lowerDateOfBirth).append("'");
+        }
+        else if (upperDateOfBirth != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`date_of_birth` <= '").append(lowerDateOfBirth).append("'");
+        }
+
+        if (city != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`city` = '").append(city).append("'");
+        }
+        if (street != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`street` = '").append(street).append("'");
+        }
+        if (home != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`home` = '").append(home).append("'");
+        }
+        if (flat != null){
+            if (builder.length() != 0){
+                builder.append(" AND ");
+            }
+            builder.append("`flat` = ").append(flat).append("'");
+        }
+
+        builder.insert(0, "SELECT * FROM `contact` where ");
+        Query query = session.createSQLQuery(builder.toString()).addEntity(Contact.class);
+        contacts = (List<Contact>) query.list();
+    }
+    finally {
+        if (session != null && session.isOpen()) {
+            session.close();
+        }
+    }
+    return contacts;
+}
 }
