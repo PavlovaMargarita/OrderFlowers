f4cec15af1411d648f0a2cf9e224ef54225665e3
VitaliPark <vitalipark@gmail.com>
2014-09-10 11:19:04 +0300

    LoginDTO and getUserInfo

--- Server/src/main/java/com/itechart/courses/controller/BasicController.java
+++ Server/src/main/java/com/itechart/courses/controller/BasicController.java
@@ -1,12 +1,16 @@
 package com.itechart.courses.controller;
 
 import com.google.gson.Gson;
+import com.itechart.courses.dto.LoginDTO;
 import com.itechart.courses.entity.Contact;
 import com.itechart.courses.dto.UserDTO;
+import com.itechart.courses.enums.RoleEnum;
 import com.itechart.courses.service.authorization.*;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +19,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -32,10 +37,16 @@ public class BasicController {
         return toJson(userDTO);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getContacts")
-    public @ResponseBody List<Contact> getContacts(){
-        return null;
+    @RequestMapping(method = RequestMethod.GET, value = "/userInfo")
+    @ResponseBody
+    public LoginDTO currentUserInfo(Authentication authentication){
+        List<GrantedAuthority> authority = (List<GrantedAuthority>) authentication.getAuthorities();
+        String stringRole = authority.get(0).getAuthority();
+        String username = authentication.getName();
+        LoginDTO loginDTO = new LoginDTO(RoleEnum.valueOf(stringRole), username);
+        return loginDTO;
     }
+
     //преобразование json-строки в объект, который хранит пришедшие параметры
 
     private JSONObject fromJson(HttpServletRequest request) throws IOException, JSONException {
@@ -53,6 +64,8 @@ public class BasicController {
         Gson gson = new Gson();
         return gson.toJson(object);
     }
+
+
 
 
 }
