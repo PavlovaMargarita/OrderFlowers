78a4bcefdb7ba5c744af6a5634a4ee63217b9446
VitaliPark <vitalipark@gmail.com>
2014-09-17 16:08:47 +0300

    roles constant

---
+++ Server/src/main/java/com/itechart/courses/enums/Roles.java
@@ -0,0 +1,13 @@
+package com.itechart.courses.enums;
+
+/**
+ * Created by Admin on 17.09.2014.
+ */
+public class Roles {
+
+    public static final String RECEIVING_ORDERS_MANAGER = "ROLE_RECEIVING_ORDERS_MANAGER";
+    public static final String PROCESSING_ORDERS_SPECIALIST = "ROLE_PROCESSING_ORDERS_SPECIALIST";
+    public static final String SERVICE_DELIVERY_MANAGER = "ROLE_SERVICE_DELIVERY_MANAGER";
+    public static final String SUPERVISOR = "ROLE_SUPERVISOR";
+    public static final String ADMIN = "ROLE_ADMIN";
+}
