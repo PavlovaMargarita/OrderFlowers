623ae841da6d40a09203ad298b59224593a43d6e
Sasha_Morozov <sasha25121993@mail.ru>
2014-09-23 12:48:02 +0300

    добавил AutoComplete для получателя

--- Client/src/main/webapp/script/jQuery.js
+++ Client/src/main/webapp/script/jQuery.js
@@ -20,4 +20,25 @@ $(function() {
             $( "#customer-hidden-id" ).val( ui.item.id);
         }
     });
+    $( "#recipient-id" ).autocomplete({
+        source: function( request, response ) {
+            $.ajax({
+                url: "/OrderFlowers/getPerson",
+                dataType: "json",
+                data: {term: request.term},
+                success: function(data) {
+                    response($.map(data, function(item) {
+                        return {
+                            value: item.surname + " " + item.name + " " + item.patronymic,
+                            id: item.id
+                        };
+                    }));
+                }
+            });
+        },
+        minLength: 2,
+        select: function( event, ui ) {
+            $( "#recipient-hidden-id" ).val( ui.item.id);
+        }
+    });
 });
