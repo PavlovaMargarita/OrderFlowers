aecd911e127e445f623da65cf7c754646e809f94
VitaliPark <vitalipark@gmail.com>
2014-09-26 09:42:12 +0300

    contact search via criteria API

--- Server/src/main/java/com/itechart/courses/dao/contact/ContactDAOImpl.java
+++ Server/src/main/java/com/itechart/courses/dao/contact/ContactDAOImpl.java
@@ -6,14 +6,12 @@ import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Expression;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.sql.Date;
 import java.util.List;
 
@@ -106,130 +104,13 @@ public class ContactDAOImpl implements ContactDAO {
         if (parameters == null) {
             throw new NullPointerException("parameters is null");
         }
-        Session session = null;
-        List<Contact> contacts = null;
-        StringBuilder builder = new StringBuilder();
-        String surname = parameters.getSurname();
-        if (surname != null && (surname = surname.trim()).isEmpty()){
-            surname = null;
-        }
-        String name = parameters.getName();
-        if (name != null && (name = name.trim()).isEmpty()){
-            name = null;
-        }
-        String patronymic = parameters.getPatronymic();
-        if (patronymic != null && (patronymic = patronymic.trim()).isEmpty()){
-            patronymic = null;
-        }
-        Date lowerDateOfBirth = parameters.getLowerDateOfBirth();
-        Date upperDateOfBirth = parameters.getUpperDateOfBirth();
-        String city = parameters.getCity();
-        if (city != null && (city = city.trim()).isEmpty()){
-            city = null;
-        }
-        String street = parameters.getStreet();
-        if (street != null && (street = street.trim()).isEmpty()){
-            street = null;
-        }
-        Integer home = parameters.getHome();
-        Integer flat = parameters.getFlat();
-
-        if (surname != null) {
-            builder.append("contact.surname LIKE :surname");
-        }
-        if (name != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.name LIKE :name");
-        }
-        if (patronymic != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.patronymic LIKE :patronymic");
-        }
-
-        if (lowerDateOfBirth != null && upperDateOfBirth != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.dateOfBirth BETWEEN :lowerDateOfBirth AND :upperDateOfBirth");
-        } else if (lowerDateOfBirth != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.dateOfBirth >= :lowerDateOfBirth");
-        } else if (upperDateOfBirth != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.dateOfBirth <= :upperDateOfBirth");
-        }
-
-        if (city != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.city LIKE :city");
-        }
-        if (street != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.street LIKE :street");
-        }
-        if (home != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.home = :home");
-        }
-        if (flat != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.flat = :flat");
-        }
-        builder.insert(0, "from Contact contact where ");
-        builder.append(" AND contact.isDelete = false");
-
-        session = sessionFactory.getCurrentSession();
-        Query query = session.createQuery(builder.toString());
-        if (surname != null) {
-            query.setString("surname", "%" + surname + "%");
-        }
-        if (name != null) {
-            query.setString("name", "%" + name + "%");
-        }
-        if (patronymic != null) {
-            query.setString("patronymic", "%" + patronymic + "%");
-        }
-
-        if (lowerDateOfBirth != null && upperDateOfBirth != null) {
-            query.setDate("lowerDateOfBirth", lowerDateOfBirth);
-            query.setDate("upperDateOfBirth", upperDateOfBirth);
-        } else if (lowerDateOfBirth != null) {
-            query.setDate("lowerDateOfBirth", lowerDateOfBirth);
-        } else if (upperDateOfBirth != null) {
-            query.setDate("upperDateOfBirth", upperDateOfBirth);
-        }
-
-        if (city != null) {
-            query.setString("city", "%" + city + "%");
-        }
-        if (street != null) {
-            query.setString("street", "%" + street + "%");
-        }
-        if (home != null) {
-            query.setInteger("home", home);
-        }
-        if (flat != null) {
-            query.setInteger("flat", flat);
-        }
-        query.setFirstResult(first);
-        query.setMaxResults(count);
-        contacts = (List<Contact>) query.list();
+        List<Contact> contacts;
+        Session session = sessionFactory.getCurrentSession();
+        Criteria criteria = session.createCriteria(Contact.class);
+        buildContactSearchCriteria(criteria, parameters);
+        criteria.setFirstResult(first);
+        criteria.setMaxResults(count);
+        contacts = (List<Contact>) criteria.list();
         return contacts;
     }
 
@@ -238,128 +119,11 @@ public class ContactDAOImpl implements ContactDAO {
         if (parameters == null) {
             throw new NullPointerException("parameters is null");
         }
-        Session session = null;
-        List<Contact> contacts = null;
-        StringBuilder builder = new StringBuilder();
-        String surname = parameters.getSurname();
-        if (surname != null && (surname = surname.trim()).isEmpty()){
-            surname = null;
-        }
-        String name = parameters.getName();
-        if (name != null && (name = name.trim()).isEmpty()){
-            name = null;
-        }
-        String patronymic = parameters.getPatronymic();
-        if (patronymic != null && (patronymic = patronymic.trim()).isEmpty()){
-            patronymic = null;
-        }
-        Date lowerDateOfBirth = parameters.getLowerDateOfBirth();
-        Date upperDateOfBirth = parameters.getUpperDateOfBirth();
-        String city = parameters.getCity();
-        if (city != null && (city = city.trim()).isEmpty()){
-            city = null;
-        }
-        String street = parameters.getStreet();
-        if (street != null && (street = street.trim()).isEmpty()){
-            street = null;
-        }
-        Integer home = parameters.getHome();
-        Integer flat = parameters.getFlat();
-
-        if (surname != null) {
-            builder.append("contact.surname LIKE :surname");
-        }
-        if (name != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.name LIKE :name");
-        }
-        if (patronymic != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.patronymic LIKE :patronymic");
-        }
-
-        if (lowerDateOfBirth != null && upperDateOfBirth != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.dateOfBirth BETWEEN :lowerDateOfBirth AND :upperDateOfBirth");
-        } else if (lowerDateOfBirth != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.dateOfBirth >= :lowerDateOfBirth");
-        } else if (upperDateOfBirth != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.dateOfBirth <= :upperDateOfBirth");
-        }
-
-        if (city != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.city LIKE :city");
-        }
-        if (street != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.street LIKE :street");
-        }
-        if (home != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.home = :home");
-        }
-        if (flat != null) {
-            if (builder.length() != 0) {
-                builder.append(" AND ");
-            }
-            builder.append("contact.flat = :flat");
-        }
-        builder.insert(0, "from Contact contact where ");
-        builder.append(" AND contact.isDelete = false");
-
-        session = sessionFactory.getCurrentSession();
-        Query query = session.createQuery(builder.toString());
-        if (surname != null) {
-            query.setString("surname", "%" + surname + "%");
-        }
-        if (name != null) {
-            query.setString("name", "%" + name + "%");
-        }
-        if (patronymic != null) {
-            query.setString("patronymic", "%" + patronymic + "%");
-        }
-
-        if (lowerDateOfBirth != null && upperDateOfBirth != null) {
-            query.setDate("lowerDateOfBirth", lowerDateOfBirth);
-            query.setDate("upperDateOfBirth", upperDateOfBirth);
-        } else if (lowerDateOfBirth != null) {
-            query.setDate("lowerDateOfBirth", lowerDateOfBirth);
-        } else if (upperDateOfBirth != null) {
-            query.setDate("upperDateOfBirth", upperDateOfBirth);
-        }
-
-        if (city != null) {
-            query.setString("city", "%" + city + "%");
-        }
-        if (street != null) {
-            query.setString("street", "%" + street + "%");
-        }
-        if (home != null) {
-            query.setInteger("home", home);
-        }
-        if (flat != null) {
-            query.setInteger("flat", flat);
-        }
-        contacts = (List<Contact>) query.list();
+        List<Contact> contacts;
+        Session session = sessionFactory.getCurrentSession();
+        Criteria criteria = session.createCriteria(Contact.class);
+        buildContactSearchCriteria(criteria, parameters);
+        contacts = (List<Contact>) criteria.list();
         return contacts;
     }
 
@@ -383,26 +147,35 @@ public class ContactDAOImpl implements ContactDAO {
         return totalCount;
     }
 
-    private void addRestrictionIfNotNull(Criteria criteria, String propertyName, Object value) {
-        if (value != null) {
-            criteria.add(Restrictions.like(propertyName, (String)value, MatchMode.ANYWHERE));
-        }
-    }
-
     private void buildContactSearchCriteria(Criteria criteria, ContactSearchDTO parameters){
         String surname = parameters.getSurname();
+        if (surname != null && (surname = surname.trim()).isEmpty()){
+            surname = null;
+        }
         String name = parameters.getName();
+        if (name != null && (name = name.trim()).isEmpty()){
+            name = null;
+        }
         String patronymic = parameters.getPatronymic();
+        if (patronymic != null && (patronymic = patronymic.trim()).isEmpty()){
+            patronymic = null;
+        }
         Date lowerDateOfBirth = parameters.getLowerDateOfBirth();
         Date upperDateOfBirth = parameters.getUpperDateOfBirth();
         String city = parameters.getCity();
+        if (city != null && (city = city.trim()).isEmpty()){
+            city = null;
+        }
         String street = parameters.getStreet();
+        if (street != null && (street = street.trim()).isEmpty()){
+            street = null;
+        }
         Integer home = parameters.getHome();
         Integer flat = parameters.getFlat();
 
-        addRestrictionIfNotNull(criteria, "surname", surname);
-        addRestrictionIfNotNull(criteria, "name", name);
-        addRestrictionIfNotNull(criteria, "patronymic", patronymic);
+        addStringRestrictionIfNotNull(criteria, "surname", surname);
+        addStringRestrictionIfNotNull(criteria, "name", name);
+        addStringRestrictionIfNotNull(criteria, "patronymic", patronymic);
         if(lowerDateOfBirth != null && upperDateOfBirth != null){
             criteria.add(Restrictions.between("dateOfBirth", lowerDateOfBirth, upperDateOfBirth));
         } else if(lowerDateOfBirth != null){
@@ -410,11 +183,23 @@ public class ContactDAOImpl implements ContactDAO {
         } else if(upperDateOfBirth != null){
             criteria.add(Restrictions.le("dateOfBirth", upperDateOfBirth));
         }
-        addRestrictionIfNotNull(criteria, "city", city);
-        addRestrictionIfNotNull(criteria, "street", street);
-        addRestrictionIfNotNull(criteria, "home", home);
-        addRestrictionIfNotNull(criteria, "flat", flat);
+        addStringRestrictionIfNotNull(criteria, "city", city);
+        addStringRestrictionIfNotNull(criteria, "street", street);
+        addIntegerRestrictionIfNotNull(criteria, "home", home);
+        addIntegerRestrictionIfNotNull(criteria, "flat", flat);
         criteria.add(Restrictions.eq("isDelete", false));
+    }
+
+    private void addStringRestrictionIfNotNull(Criteria criteria, String propertyName, String value) {
+        if (value != null) {
+            criteria.add(Restrictions.like(propertyName, value, MatchMode.ANYWHERE));
+        }
+    }
+
+    private void addIntegerRestrictionIfNotNull(Criteria criteria, String propertyName, Integer value) {
+        if (value != null) {
+            criteria.add(Restrictions.eq(propertyName, value));
+        }
     }
 
 }
