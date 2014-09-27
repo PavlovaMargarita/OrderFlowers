3833110c688482297bab51aed699f554ba7f097c
VitaliPark <vitalipark@gmail.com>
2014-09-27 17:42:22 +0300

    totalPageNumber method fixed

--- Client/src/main/webapp/script/app.js
+++ Client/src/main/webapp/script/app.js
@@ -21,7 +21,10 @@ app.run(function($rootScope, $cookieStore){
 
 app.service('PagerService', function() {
     this.totalPageNumber = function(pageRecords, totalRecords) {
-        var totalPageNumber = Math.floor((totalRecords + pageRecords - 1) / pageRecords);
+        var totalPageNumber = 1;
+        if(typeof totalRecords != 'undefined'){
+            totalPageNumber = Math.floor((totalRecords + pageRecords - 1) / pageRecords);
+        }
         return (totalPageNumber == 0) ? 1 : totalPageNumber;
     }
 
